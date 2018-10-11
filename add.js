69830fc57ae5a69cccc4b4176c97202eac6dde93
MedEye Demo <medeyedemo@clients-pool-1.nat.ru.is>
2018-10-11 16:09:50 +0000

    1. String Calculator: fyrsta commit

---
+++ add.js
@@ -0,0 +1,18 @@
+// add.js
+
+function Add(numbers)
+{  
+    var x = "0";
+    var y = "1";
+    var z = "2";
+    
+    var num1 = parseInt(x);
+    var num2 = parseInt(y);
+    var num3 = parseInt(z);
+    
+    var sum = num1 + num2 + num3;
+    
+    console.log(sum);
+}
+  
+Add();
