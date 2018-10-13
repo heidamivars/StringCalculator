64e976a8e4a55ec2b61d36811845d7d74c7b2e38
MedEye Demo <medeyedemo@MedEyes-MacBook-Pro.local>
2018-10-13 17:24:36 +0000

    An empty string will return 0

--- add.js
+++ add.js
@@ -1,18 +1,10 @@
 // add.js
 
-function Add(numbers)
-{  
-    var x = "0";
-    var y = "1";
-    var z = "2";
-    
-    var num1 = parseInt(x);
-    var num2 = parseInt(y);
-    var num3 = parseInt(z);
-    
-    var sum = num1 + num2 + num3;
-    
-    console.log(sum);
+function add(numbers){
+    if(typeof numbers === 'null'){
+        return 0; //return 0 if input has no value
+    }
+    return numbers.length; //return lenght of string
 }
-  
-Add();
+
+console.log(add(""));
