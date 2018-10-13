bb8092808e3e12c1768473ae91f09f7c4b206d08
MedEye Demo <medeyedemo@MedEyes-MacBook-Pro.local>
2018-10-13 21:19:19 +0000

    liður 1 lagaður: kommur eru leyfðar

--- add.js
+++ add.js
@@ -1,22 +1,25 @@
 // add.js
 
-function add(numbers){
-    if(typeof numbers === '""' || undefined ){
-        return 0; //return 0 if input has no value
-    }
-    
-    
-    var sum = 0;
-    
-    do{
-        sum = sum + numbers%10;
-        numbers = Math.floor(numbers/10)
-        
-    } while(numbers);
-      return parseInt(sum);
-    
-    //reiknar summu inputs en það er ekki hægt að hafa kommuna með hérna... 
-
+if(typeof StringCalculator == 'undefined') {
+	StringCalculator = {}; //Undefined þegar string er tómur
 }
 
-console.log(add("12345")); //þetta höndlar unknown amount of numbers nú þegar en hvernig er hægt að leyfa bara 3 tölur af stærðinni 0,1,2?
+function add(numbers){
+    		var result = 0;
+		
+		if(0 === numbers.length)
+			return result;   //returnar 0 þegar string er tómur
+			
+		var inputs = numbers.split(',');  //svo kommur sé ok
+		
+		for (var i = 0; i < inputs.length; i++) {
+			result += parseInt(inputs[i]);
+		}
+		
+		return result;
+
+
+};
+ 
+console.log(add("1,2"));
+    
